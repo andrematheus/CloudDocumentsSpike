35a8284980d703cb029c99ad1440becadca32b1e
André Roque Matheus <amatheus@ligpo.dev>
2020-11-17 23:20:04 -0300

    Initial Commit

---
+++ Shared/CloudDocumentsSpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudDocumentsSpikeApp.swift
+//  Shared
+//
+//  Created by André Roque Matheus on 17/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CloudDocumentsSpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
